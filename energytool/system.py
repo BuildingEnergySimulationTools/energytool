fc02688ffd83ac0449aba5af60930811fa60679d
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2022-04-26 15:41:12 +0200

    System AuxiliarySimplified

--- energytool/system.py
+++ energytool/system.py
@@ -36,5 +36,37 @@ class GasBoiler:
         ], axis=1)
 
 
+class AuxiliarySimplified:
+    """
+    Simplified heating auxiliary component energy consumption.
+    multiply ideal heat need by a constant. Default 5%
+    """
+    def __init__(self, name, building, zones="*", ratio=0.05):
+        self.name = name
+        self.building = building
+        self.zones = zones
+        self.ratio = ratio
+
+    def pre_process(self):
+        add_output_zone_variable(
+            idf=self.building.idf,
+            zones=self.zones,
+            variables="Zone Ideal Loads Supply Air Total Heating Energy"
+        )
+
+    def post_process(self):
+        ideal_heating = get_output_zone_variable(
+            eplus_res=self.building.energyplus_results,
+            zones=self.zones,
+            variables="Zone Ideal Loads Supply Air Total Heating Energy"
+        )
+
+        system_out = (ideal_heating * self.ratio).sum(axis=1)
+        system_out.name = f"{self.name}_Energy"
+
+        self.building.building_results = pd.concat([
+            self.building.building_results,
+            system_out
+        ], axis=1)
 
 
