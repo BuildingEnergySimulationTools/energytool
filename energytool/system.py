e0195979211244a72ec28a2a20f146c65d806a24
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2022-04-26 18:28:38 +0200

    System AirHandlingUnit in dev

--- energytool/system.py
+++ energytool/system.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
-from energytool.epluspreprocess import add_output_zone_variable
-from energytool.epluspostprocess import get_output_zone_variable
+import energytool.epluspreprocess as pr
+import energytool.epluspostprocess as po
 
 
 class GasBoiler:
-    def __init__(self, name, building, zones="*", cop=0.86, energy="gaz", cost=0):
+    def __init__(self, name, building, zones="*", cop=0.86, energy="gaz",
+                 cost=0):
         self.name = name
         self.building = building
         self.cop = cop
@@ -14,14 +15,14 @@ class GasBoiler:
         self.zones = zones
 
     def pre_process(self):
-        add_output_zone_variable(
+        pr.add_output_zone_variable(
             idf=self.building.idf,
             zones=self.zones,
             variables="Zone Ideal Loads Supply Air Total Heating Energy"
         )
 
     def post_process(self):
-        ideal_heating = get_output_zone_variable(
+        ideal_heating = po.get_output_zone_variable(
             eplus_res=self.building.energyplus_results,
             zones=self.zones,
             variables="Zone Ideal Loads Supply Air Total Heating Energy"
@@ -41,6 +42,7 @@ class AuxiliarySimplified:
     Simplified heating auxiliary component energy consumption.
     multiply ideal heat need by a constant. Default 5%
     """
+
     def __init__(self, name, building, zones="*", ratio=0.05):
         self.name = name
         self.building = building
@@ -48,14 +50,14 @@ class AuxiliarySimplified:
         self.ratio = ratio
 
     def pre_process(self):
-        add_output_zone_variable(
+        pr.add_output_zone_variable(
             idf=self.building.idf,
             zones=self.zones,
             variables="Zone Ideal Loads Supply Air Total Heating Energy"
         )
 
     def post_process(self):
-        ideal_heating = get_output_zone_variable(
+        ideal_heating = po.get_output_zone_variable(
             eplus_res=self.building.energyplus_results,
             zones=self.zones,
             variables="Zone Ideal Loads Supply Air Total Heating Energy"
@@ -70,3 +72,71 @@ class AuxiliarySimplified:
         ], axis=1)
 
 
+class AirHandlingUnit:
+    """
+    If "ach" argument is used, DesignSpecification:OutdoorAir objects Names
+    corresponding to specified "zones" must contain zones Name
+    in their "Name" field
+    """
+
+    def __init__(self,
+                 name,
+                 building,
+                 zones='*',
+                 fan_energy_coefficient=0.23,
+                 heat_recovery_efficiency=0,
+                 ach=None):
+
+        self.name = name
+        self.building = building
+        self.zones = zones
+        self.ach = ach
+        self.fan_energy_coefficient = fan_energy_coefficient
+        self.heat_recovery_efficiency = heat_recovery_efficiency
+
+    def pre_process(self):
+        pr.add_output_zone_variable(
+            idf=self.building.idf,
+            zones=self.zones,
+            variables=
+            "Zone Mechanical Ventilation Standard Density Volume Flow Rate"
+        )
+
+        if self.ach is not None:
+            if self.zones == "*":
+                obj_name_arg = None
+            else:
+                design_list = self.building.idf.idfojects[
+                    "DesignSpecification:OutdoorAir"]
+                obj_name_arg = []
+                for zn in self.building.zone_name_list:
+                    for obj in design_list:
+                        if zn in obj.Name:
+                            obj_name_arg.append(obj.Name)
+
+                pr.set_object_field_value(
+                    idf=self.building.idf,
+                    idf_object="DesignSpecification:OutdoorAir",
+                    idf_object_name=obj_name_arg,
+                    field_name="Outdoor_Air_Flow_Air_Changes_per_Hour",
+                    value=self.ach
+                )
+
+    def post_process(self):
+        air_volume = po.get_output_zone_variable(
+            eplus_res=self.building.energyplus_results,
+            zones=self.zones,
+            variables=
+            "Zone Mechanical Ventilation Standard Density Volume Flow Rate"
+        )
+
+        system_out = (
+                air_volume * 3600 * self.fan_energy_coefficient
+        ).sum(axis=1)
+
+        system_out.name = f"{self.name}_Energy"
+
+        self.building.building_results = pd.concat([
+            self.building.building_results,
+            system_out
+        ], axis=1)
