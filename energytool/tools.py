3d225ed76c19c72eaf4207b27d771569fbf048d4
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2022-05-06 17:20:25 +0200

    Tools Scheduler

--- energytool/tools.py
+++ energytool/tools.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import datetime as dt
+
+
 def format_input_to_list(f_input):
     """
     Convert a string into a list
@@ -28,3 +32,43 @@ def select_by_strings(items_list, select_by):
                 output_list.append(items)
 
     return output_list
+
+
+def hourly_lst_from_dict(hourly_dict):
+    if list(hourly_dict.keys())[-1] != 24:
+        raise ValueError("Last dict key must be 24")
+
+    val_list = []
+    for hour, val in hourly_dict.items():
+        val_list += [val for _ in range(len(val_list), hour)]
+
+    return val_list
+
+
+class Scheduler:
+    def __init__(self, name, year=dt.datetime.today().year):
+        self.name = name
+        self.year = year
+        self.series = pd.Series(
+            index=pd.date_range(
+                f'{year}-01-01 00:00:00', freq="H", periods=8760),
+            name=name,
+            dtype='float64'
+        )
+
+    def add_day_in_period(self, start, end, days, hourly_dict):
+        start = dt.datetime.strptime(start, '%Y-%m-%d')
+        end = dt.datetime.strptime(end, '%Y-%m-%d')
+        end = end.replace(hour=23)
+
+        if start.year != self.year or end.year != end.year:
+            raise ValueError("start date or end date is out of bound ")
+
+        day_list = format_input_to_list(days)
+        period = self.series.loc[start: end]
+
+        selected_timestamp = [idx for idx in period.index
+                              if idx.day_name() in day_list]
+
+        self.series.loc[selected_timestamp] = hourly_lst_from_dict(
+            hourly_dict) * int(len(selected_timestamp) / 24)
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -25,3 +25,56 @@ class TestTools:
 
         assert to_test == ['Zone_1:control', 'control:Zone_1',
                            'Zone_2:control', 'control:Zone_2control']
+
+    def test_hourly_lst_from_dict(self):
+        day_config = {
+            6: 15,
+            18: 19,
+            24: 15
+        }
+
+        ref = [15, 15, 15, 15, 15, 15, 19, 19, 19, 19, 19, 19,
+               19, 19, 19, 19, 19, 19, 15, 15, 15, 15, 15, 15]
+
+        to_test = tl.hourly_lst_from_dict(day_config)
+
+        assert ref == to_test
+
+    def test_scheduler(self):
+        week_day = {
+            6: 15,
+            18: 19,
+            24: 15
+        }
+        weekend = {24: 19}
+        summer = {24: -50}
+
+        test_scheduler = tl.Scheduler(name="test", year=2009)
+        test_scheduler.add_day_in_period(
+            start="2009-01-01",
+            end="2009-12-31",
+            days=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+            hourly_dict=week_day)
+
+        test_scheduler.add_day_in_period(
+            start="2009-01-01",
+            end="2009-12-31",
+            days=['Saturday', 'Sunday'],
+            hourly_dict=weekend)
+
+        test_scheduler.add_day_in_period(
+            start="2009-04-01",
+            end="2009-09-30",
+            days=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+                  'Saturday', 'Sunday'],
+            hourly_dict=summer
+        )
+
+        ref = [15.0, 15.0, 15.0, 15.0, 15.0, 15.0, 19.0, 19.0, 19.0, 19.0,
+               19.0, 19.0, 19.0, 19.0, 19.0, 19.0, 19.0, 19.0, 15.0, 15.0,
+               15.0, 15.0, 15.0, 15.0, 19.0, 19.0, 19.0, 19.0, 19.0, 19.0,
+               19.0, 19.0, 19.0, 19.0, 19.0, 19.0, 19.0, 19.0, 19.0, 19.0,
+               19.0, 19.0, 19.0, 19.0, 19.0, 19.0, 19.0, 19.0]
+
+        assert ref == list(
+            test_scheduler.series.loc["2009-01-02":"2009-01-03"])
