91c6a4d7bc2d9b378034f652f39e3b17ded32302
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2022-05-16 17:54:15 +0200

    PARAMETER adding absolute attribute. Modifying building parameter specification

--- energytool/parameter.py
+++ energytool/parameter.py
@@ -7,24 +7,61 @@ class UncertainParameter:
                  building,
                  bounds,
                  idf_parameters=None,
-                 building_parameters=None):
+                 building_parameters=None,
+                 absolute=False):
         self.name = name
         self.building = building
         self.bounds = bounds
         self.idf_parameters = idf_parameters
         self.building_parameters = building_parameters
+        self.absolute = absolute
+        if idf_parameters is not None:
+            self.idf_nominal_values = [
+                pr.get_objects_field_values(
+                    idf=self.building.idf,
+                    idf_object=element["idf_object"],
+                    idf_object_names=element["names"],
+                    field_name=element["field"]
+                )
+                for element in idf_parameters
+            ]
+        else:
+            self.idf_nominal_values = []
+
+        if building_parameters is not None:
+            self.building_nominal_values = [
+                getattr(
+                    getattr(building, element['category'])[element["element_name"]],
+                    element["key"])
+                for element in building_parameters
+            ]
+        else:
+            self.building_nominal_values = []
 
     def set_value(self, value):
         if self.idf_parameters is not None:
-            for element in self.idf_parameters:
+            for element, nominal_values in zip(
+                    self.idf_parameters, self.idf_nominal_values):
+                if not self.absolute:
+                    values_to_set = [val * value for val in nominal_values]
+                else:
+                    values_to_set = value
+
                 pr.set_objects_field_values(
                     self.building.idf,
                     idf_object=element["idf_object"],
                     idf_object_names=element["names"],
                     field_name=element["field"],
-                    values=value
+                    values=values_to_set
                 )
         if self.building_parameters is not None:
-            for element in self.building_parameters:
-                setattr(element["category"][element["element_name"]],
-                        element["key"], value)
+            for element, nominal_value in zip(
+                    self.building_parameters, self.building_nominal_values):
+                if not self.absolute:
+                    value_to_set = value * nominal_value
+                else:
+                    value_to_set = value
+                setattr(getattr(self.building, element["category"])[
+                            element["element_name"]],
+                        element["key"],
+                        value_to_set)
--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from copy import deepcopy
 
 import energytool.system as st
 
@@ -18,11 +19,14 @@ class TestParameter:
     def test_uncertain_parameter(self):
         building = Building(idf_path=RESOURCES_PATH / "test.idf")
         heater = st.HeaterSimple(name="Heater", building=building)
+        ahu = st.AirHandlingUnit(name="AHU", building=building)
         building.heating_system = {heater.name: heater}
+        building.ventilation_system = {ahu.name: ahu}
 
+        building_1 = deepcopy(building)
         uncertain_parameter = UncertainParameter(
             name='test',
-            building=building,
+            building=building_1,
             bounds=[0, 1],
             idf_parameters=[dict(
                 idf_object="Material",
@@ -30,16 +34,60 @@ class TestParameter:
                 field="Conductivity"
             )],
             building_parameters=[dict(
-                category=building.heating_system,
+                category="heating_system",
                 element_name="Heater",
                 key="cop"
-            )]
+            )],
+            absolute=True
         )
 
         uncertain_parameter.set_value(value=42)
 
         ref_idf = [element.Conductivity
-                   for element in building.idf.idfobjects["Material"][2:4]]
+                   for element in building_1.idf.idfobjects["Material"][2:4]]
 
         assert ref_idf == [42] * 2
-        assert building.heating_system['Heater'].cop == 42
+        assert building_1.heating_system['Heater'].cop == 42
+
+        building_2 = deepcopy(building)
+        uncertain_parameter = UncertainParameter(
+            name='test',
+            building=building_2,
+            bounds=[0, 1],
+            idf_parameters=[
+                dict(
+                    idf_object="Material",
+                    names=['Floor/Roof Screed_.03',
+                           'Cast Concrete (Dense)_.1'],
+                    field="Conductivity"
+                ),
+                dict(
+                    idf_object="Material",
+                    names=['Gypsum Plasterboard_.025',
+                           'Project medium concrete block_.2'],
+                    field="Conductivity"
+                ),
+            ],
+            building_parameters=[
+                dict(
+                    category="heating_system",
+                    element_name="Heater",
+                    key="cop"
+                ),
+                dict(
+                    category="ventilation_system",
+                    element_name="AHU",
+                    key="fan_energy_coefficient"
+                ),
+            ],
+            absolute=False
+        )
+
+        uncertain_parameter.set_value(value=42)
+
+        ref_idf = [element.Conductivity
+                   for element in building_2.idf.idfobjects["Material"][2:6]]
+
+        assert ref_idf == [17.22, 58.8, 10.5, 21.42]
+        assert building_2.heating_system['Heater'].cop == 0.86 * 42
+        assert building_2.ventilation_system['AHU'].fan_energy_coefficient == 0.23 * 42
