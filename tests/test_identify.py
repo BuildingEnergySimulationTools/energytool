459a7f81f593d8833a69a8b1e48cdc27e9632f5e
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2022-12-07 18:56:34 +0100

    test_identify.py test error_func_with_gaps

--- tests/test_identify.py
+++ tests/test_identify.py
@@ -28,7 +28,7 @@ class TestIdentify:
         )
 
         gaps_list = [('2009-01-01 00:00:00', '2009-01-01 01:00:00'),
-                     ('2009-01-01 06:00:00', '2009-01-01 09:00:00'),]
+                     ('2009-01-01 06:00:00', '2009-01-01 09:00:00')]
 
         assert error_func_with_gaps(results, reference, gaps_list) == 0
 
@@ -62,13 +62,18 @@ class TestIdentify:
             parameters=param_list
         )
 
+        gaps_list = [('2009-01-01 00:00:00', '2009-01-01 01:00:00'),
+                     ('2009-01-01 06:00:00', '2009-01-01 09:00:00')]
+
         id_obj.fit(
             reference=reference,
             epw_file_path=RESOURCES_PATH / "Paris_2020.epw",
             simulation_start=dt.datetime(2009, 1, 1, 0, 0, 0),
             simulation_stop=dt.datetime(2009, 1, 1, 23, 0, 0),
             calibration_timestep=dt.timedelta(hours=2),
-            indicator='Old_boiler_Energy',
+            indicator='Old_boiler_Energy_[J]',
+            error_function=error_func_with_gaps,
+            err_func_args={"gaps_list": gaps_list},
             convergence_tolerance=0.2)
 
         assert round(id_obj.parameters_id_values["Boiler_COP"], 2) == 0.5
