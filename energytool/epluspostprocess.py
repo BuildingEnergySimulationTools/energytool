b858d27b94a4e93474f7601ded40ceb2b662a82b
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2022-12-21 12:00:21 +0100

    epluspostprocess.py get_aggregated_indicator

--- energytool/epluspostprocess.py
+++ energytool/epluspostprocess.py
@@ -93,3 +93,47 @@ def get_output_variable(
         results.columns = new_columns
 
     return results
+
+
+def get_aggregated_indicator(simulation_list,
+                             results_group='building_results',
+                             indicator='Total',
+                             method=np.sum,
+                             method_args=None,
+                             reference=None):
+    if not simulation_list:
+        raise ValueError("Empty simulation list. "
+                         "Cannot perform indicator aggregation")
+
+    first_build = simulation_list[0].building
+    available = list(first_build.building_results.columns)
+    available += list(first_build.energyplus_results.columns)
+    available.append("Total")
+
+    if indicator not in available:
+        raise ValueError("Indicator is not present in building_results or "
+                         "in energyplus_results")
+
+    y_df = pd.concat([
+        getattr(sim.building, results_group)[indicator]
+        for sim in simulation_list
+    ], axis=1)
+
+    if reference is None:
+        return method(y_df).to_numpy()
+
+    elif method_args is None:
+        return np.array([
+            method(reference, y_df.iloc[:, i])
+            for i in range(y_df.shape[1])
+        ])
+
+    else:
+        return np.array([
+            method(reference, y_df.iloc[:, i], **method_args)
+            for i in range(y_df.shape[1])
+        ])
+
+
+
+
