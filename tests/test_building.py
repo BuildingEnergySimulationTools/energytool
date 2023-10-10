816613237d495cf4dcdbc24b4e49af81f58d97fd
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2023-10-10 17:09:13 +0200

    ✅

--- tests/test_building.py
+++ tests/test_building.py
@@ -8,26 +8,25 @@ RESOURCES_PATH = Path(__file__).parent / "resources"
 
 Building.set_idd(RESOURCES_PATH)
 
-PARAM_DICT = {
-    "idf.material.Urea Formaldehyde Foam_.1327.Conductivity": 0.05,
-    "system.heating.Heater.cop": 0.5,
-    "epw_file": (
-        Path(r"C:\EnergyPlusV9-4-0\WeatherData") / "B4R_weather_Paris_2020.epw"
-    ).as_posix(),
-}
-
-SIMULATION_OPTION = {
-    SimuOpt.OUTPUTS: f"{OutputCategories.SYSTEM.value}|{OutputCategories.RAW.value}"
-}
-
 
 class TestBuilding:
     def test_load_idf(self):
         test_build = Building(idf_path=RESOURCES_PATH / "test.idf")
         test_build.add_system(HeaterSimple(name="Heater", cop=0.1))
 
+        param_dict = {
+            "idf.material.Urea Formaldehyde Foam_.1327.Conductivity": 0.05,
+            "system.heating.Heater.cop": 0.5,
+            "epw_file": (
+                Path(r"C:\EnergyPlusV9-4-0\WeatherData") / "B4R_weather_Paris_2020.epw"
+            ).as_posix(),
+        }
+        simulation_options = {
+            SimuOpt.OUTPUTS.value: f"{OutputCategories.SYSTEM.value}|{OutputCategories.RAW.value}"
+        }
+
         res = test_build.simulate(
-            parameter_dict=PARAM_DICT, simulation_options=SIMULATION_OPTION
+            parameter_dict=param_dict, simulation_options=simulation_options
         )
 
         assert test_build.zone_name_list == [
@@ -40,6 +39,35 @@ class TestBuilding:
         assert test_build.surface == 200.0
 
         assert test_build.volume == 600.0
+
+        assert res.sum().to_dict() == {
+            "HEATING_Energy_[J]": 124442595875.44434,
+            "TOTAL_SYSTEM_Energy_[J]": 124442595875.44434,
+            "BLOCK1:APPTX1W:Zone Other Equipment Total Heating Energy [J](Hourly)": 18564769403.136005,
+            "BLOCK1:APPTX1E:Zone Other Equipment Total Heating Energy [J](Hourly)": 18564769403.136005,
+            "BLOCK2:APPTX2W:Zone Other Equipment Total Heating Energy [J](Hourly)": 18564769403.136005,
+            "BLOCK2:APPTX2E:Zone Other Equipment Total Heating Energy [J](Hourly)": 18564769403.136005,
+            "BLOCK1:APPTX1W IDEAL LOADS AIR:Zone Ideal Loads Supply Air Total Heating Energy [J](Hourly)": 15412078533.53048,
+            "BLOCK1:APPTX1E IDEAL LOADS AIR:Zone Ideal Loads Supply Air Total Heating Energy [J](Hourly)": 15855121735.988373,
+            "BLOCK2:APPTX2W IDEAL LOADS AIR:Zone Ideal Loads Supply Air Total Heating Energy [J](Hourly)": 15276675722.295742,
+            "BLOCK2:APPTX2E IDEAL LOADS AIR:Zone Ideal Loads Supply Air Total Heating Energy [J](Hourly) ": 15677421945.907581,
+        }
+
+        param_dict = {
+            "idf.material.Urea Formaldehyde Foam_.1327.Conductivity": 0.05,
+            "system.heating.Heater.cop": 0.5,
+        }
+
+        simulation_options = {
+            "epw_file": (
+                Path(r"C:\EnergyPlusV9-4-0\WeatherData") / "B4R_weather_Paris_2020.epw"
+            ).as_posix(),
+            SimuOpt.OUTPUTS.value: f"{OutputCategories.SYSTEM.value}|{OutputCategories.RAW.value}",
+        }
+
+        res = test_build.simulate(
+            parameter_dict=param_dict, simulation_options=simulation_options
+        )
 
         assert res.sum().to_dict() == {
             "HEATING_Energy_[J]": 124442595875.44434,
