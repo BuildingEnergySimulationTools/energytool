4f2e8ffdededd8f64e46b67f0520ee7bc39948be
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2023-11-06 11:43:07 +0100

    ⚡️ Setup update

--- setup.py
+++ setup.py
@@ -3,6 +3,17 @@
 
 from setuptools import setup, find_packages
 
+EXTRAS_REQUIRE = {
+    "tests": [
+        "pytest==7.3.1",
+    ],
+    "lint": [
+        "pre-commit==3.3.2",
+    ],
+}
+
+EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"] + EXTRAS_REQUIRE["lint"]
+
 # Get the long description from the README file
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
@@ -12,31 +23,25 @@ setup(
     version="0.1",
     description="Tools for EnergyPlus",
     long_description=long_description,
-    # url="",
+    url="https://github.com/BuildingEnergySimulationTools/energytool",
     author="Nobatek/INEF4",
     author_email="bdurandestebe@nobatek.com",
-    # license="",
-    # keywords=[
-    # ],
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.10",
     install_requires=[
-        "numpy>=1.22.3",
-        "pandas>=1.4.2",
-        "eppy>=0.5.57",
-        "fastprogress>=1.0.2",
-        "contextvars>=2.4",
-        "plotly>=5.8.0",
-        "SALib>=1.4.5",
+        "numpy>=1.26.0",
+        "pandas>=2.1.2",
+        "eppy>=0.5.63",
+        "corrai>=0.1.0"
     ],
+    extras_require=EXTRAS_REQUIRE,
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
 )
